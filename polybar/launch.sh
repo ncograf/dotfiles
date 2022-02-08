192a9c2aec581f02d49593830c4fc4f717f5fee5
Nico Graf <ncograf@gmail.com>
2022-02-08 11:59:09 +0100

    First try to change polybar not working

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+DIR="$XDG_CONFIG_HOME/polybar"
+
 # Terminate already running bar instances
 killall -q polybar
 
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 # Launch Polybar unsing the decfault config
-polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar -q main -c "$DIR"/config.ini &
+    done
+else
+    MONITOR=$m polybar -q main -c "$DIR"/config.ini &
+fi
---
+++ polybar/scripts/launcher.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rofi -no-config -no-lazy-grab -show drun -modi drun -theme $XDG_CONFIG_HOME/polybar/scripts/rofi/launcher.rasi
